ef7b718f1e59de168bf5520ff39121a3f05041f4
AlexanderPokidin <a.pokidin@gmail.com>
2017-07-21 13:41:39 +0300

    WorkoutDefaultFragment is done

--- app/src/main/java/com/example/alex/workout/WorkoutDetailFragment.java
+++ app/src/main/java/com/example/alex/workout/WorkoutDetailFragment.java
@@ -6,6 +6,7 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -23,6 +24,19 @@ public class WorkoutDetailFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_workout_detail, container, false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        View view = getView();
+        if (view != null) {
+            TextView title = (TextView) view.findViewById(R.id.textTitle);
+            Workout workout = Workout.workouts[(int) workoutId];
+            title.setText(workout.getName());
+            TextView description = (TextView) view.findViewById(R.id.textDescription);
+            description.setText(workout.getDescrition());
+        }
     }
 
     public void setWorkout(long id){
